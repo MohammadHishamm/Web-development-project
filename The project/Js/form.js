0c09ac964f4e9e35ead94323c8b103e1999b7f86
Mohammad Hisham <mohammad2109652@miuegypt.edu.eg>
2023-03-17 18:10:04 +0200

    form val

--- The project/Js/form.js
+++ The project/Js/form.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+    
     document.querySelectorAll(".btn").forEach(function(btn) {
       btn.addEventListener("click", function() {
         document.querySelector(".form-signin").classList.toggle("form-signin-left");
@@ -12,14 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   
-    document.querySelectorAll(".btn-signup").forEach(function(btn) {
-      btn.addEventListener("click", function() {
-        document.querySelector(".nav").classList.toggle("nav-up");
-        document.querySelector(".form-signup-left").classList.toggle("form-signup-down");
-        document.querySelector(".success").classList.toggle("success-left");
-        document.querySelector(".frame").classList.toggle("frame-short");
-      });
-    });
+    
   
     document.querySelectorAll(".btn-signin").forEach(function(btn) {
       btn.addEventListener("click", function() {
@@ -32,5 +25,73 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector(".forgot").classList.toggle("forgot-fade");
       });
     });
+  
+
+  //Validation part:--------------------------------------------------
+
+let userName = document.getElementById("username");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let confirmPassword = document.getElementById("cpassword");
+let alert1=document.getElementById("alert1");
+
+
+function handleInput() {
+ 
+  let userNameValue = userName.value.trim();
+  let emailValue = email.value.trim();
+  let passwordValue = password.value.trim();
+  let confirmPasswordValue = confirmPassword.value.trim();
+
+  
+  if (userNameValue === "") {
+    setErrorFor(userName, "Username cannot be blank");
+  } else {
+    setSuccessFor(userName);
+  }
+
+  
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be blank");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Email is not valid");
+  } else {
+    setSuccessFor(email);
+  }
+
+  // Checking for password
+  if (passwordValue === "") {
+    setErrorFor(password, "Password cannot be blank");
+  } else if (passwordValue.length < 6 || passwordValue.length > 30) {
+    setErrorFor(password, "Password length should be between 6 and 30");
+  } else {
+    setSuccessFor(password);
+  }
+
+  // Checking for confirm password
+  if (confirmPasswordValue === "") {
+    setErrorFor(confirmPassword, "Confirm Password cannot be blank");
+  } else if (confirmPasswordValue !== passwordValue) {
+    setErrorFor(confirmPassword, "Confirm password not matched with password");
+  } else {
+    setSuccessFor(confirmPassword);
+  }
+}
+
+
+// To check if email is valid or not ?
+function isEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+
+  signup.addEventListener("click", function() {
+    document.querySelector(".nav").classList.toggle("nav-up");
+    document.querySelector(".form-signup-left").classList.toggle("form-signup-down");
+    document.querySelector(".success").classList.toggle("success-left");
+    document.querySelector(".frame").classList.toggle("frame-short");
+    handleInput();
   });
+
   

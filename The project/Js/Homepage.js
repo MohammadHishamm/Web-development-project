b34d3ba00021e63b994e23c056fde5848accb5a4
Mohammad Hisham <mohammad2109652@miuegypt.edu.eg>
2023-03-23 17:49:35 +0200

    width modify

--- The project/Js/Homepage.js
+++ The project/Js/Homepage.js
@@ -118,4 +118,11 @@ function showSlides() {
   setTimeout(showSlides, 4000);
 }
 
+var docWidth = document.documentElement.offsetWidth;
+[].forEach.call(document.querySelectorAll("*"), function (el) {
+  if (el.offsetWidth > docWidth) {
+    console.log(el);
+  }
+});
+
 //---------------------------offers slider--------------------------//

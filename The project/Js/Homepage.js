6db68a64c027faf9372b7bfb8e0010d6ef88a83c
Omar Hosny <omar2108721@miuegypt.edu.eg>
2023-03-23 21:57:58 +0200

    minor updates

--- The project/Js/Homepage.js
+++ The project/Js/Homepage.js
@@ -44,57 +44,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
   document.addEventListener("scroll", util.scrollEvent);
 });
 
-productScroll();
-
-function productScroll() {
-  let slider = document.getElementById("slider");
-  let next = document.getElementsByClassName("pro-next");
-  let prev = document.getElementsByClassName("pro-prev");
-  let slide = document.getElementById("slide");
-  let item = document.getElementById("slide");
-
-  for (let i = 0; i < next.length; i++) {
-    let position = 0;
-
-    prev[i].addEventListener("click", function () {
-      if (position > 0) {
-        position -= 1;
-        translateX(position);
-      }
-    });
-
-    next[i].addEventListener("click", function () {
-      if (position >= 0 && position < hiddenItems()) {
-        position += 1;
-        translateX(position);
-      }
-    });
-  }
-
-  function hiddenItems() {
-    let items = getCount(item, false);
-    let visibleItems = slider.offsetWidth / 210;
-    return items - Math.ceil(visibleItems);
-  }
-}
-
-function translateX(position) {
-  slide.style.left = position * -210 + "px";
-}
-
-function getCount(parent, getChildrensChildren) {
-  let relevantChildren = 0;
-  let children = parent.childNodes.length;
-  for (let i = 0; i < children; i++) {
-    if (parent.childNodes[i].nodeType != 3) {
-      if (getChildrensChildren)
-        relevantChildren += getCount(parent.childNodes[i], true);
-      relevantChildren++;
-    }
-  }
-  return relevantChildren;
-}
-
 /*-----------------Slideshow----------------*/
 let slideIndex = 0;
 showSlides();
